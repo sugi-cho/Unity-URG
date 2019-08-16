5f4a016a3dc6761fcb9c5b0a1f44bf0d6a759e15
Hironori Sugino <git@sugi.cc>
2019-08-16 14:20:30 +0900

    update shader

--- Assets/UrgVisualize/Scripts/UrgVisualize.cs
+++ Assets/UrgVisualize/Scripts/UrgVisualize.cs
@@ -1,22 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class UrgVisualize : MonoBehaviour
 {
+    public Renderer sensingAreaRenderer;
+    Transform sensingAreaTrs;
+    MaterialPropertyBlock mpb { get { if (_mpb == null) _mpb = new MaterialPropertyBlock(); return _mpb; } }
+    MaterialPropertyBlock _mpb;
+
     // Start is called before the first frame update
     [Header("this is for develop")]
     public List<long> distances;
+    UrgControl urgControl;
     UrgDeviceEthernet urg;
+
+    ComputeBuffer distanceDataBuffer;
+
+    [Header("props for shader")]
+    [SerializeField] Vector4 urgPos;
+    [SerializeField] Vector4 urgProps;
+
+    [Header("sensing area")]
+    public Vector2 areaSize = new Vector2(1f, 1f);
 
     void Start()
     {
+        sensingAreaTrs = sensingAreaRenderer.transform;
+        urgControl = GetComponent<UrgControl>();
         urg = GetComponent<UrgDeviceEthernet>();
+
+        distanceDataBuffer = new ComputeBuffer(urgControl.endStep - urgControl.startStep + 1, sizeof(float));
+    }
+
+    private void OnDestroy()
+    {
+        if (distanceDataBuffer != null)
+            distanceDataBuffer.Release();
     }
 
     // Update is called once per frame
     void Update()
     {
         //distances = urg.distances;
+        if (urg.distances != null && 0 < urg.distances.Count)
+        {
+            distances.Clear();
+            distances.AddRange(urg.distances);
+        }
+        var ds = distances.Select(d => d * 0.001f).ToList();
+
+        var pos = (Vector2)transform.localPosition;
+        var dir = (Vector2)sensingAreaTrs.InverseTransformDirection(transform.forward);
+        var sensorAngle = Mathf.Atan2(dir.y, dir.x);
+        urgPos.Set(pos.x, pos.y, sensorAngle, 0);
+        urgProps.Set(urgControl.endStep, urgControl.angleOffset * Mathf.Deg2Rad, urgControl.angleDelta * Mathf.Deg2Rad, 0);
+
+        sensingAreaRenderer.GetPropertyBlock(mpb);
+        distanceDataBuffer.SetData(ds);
+        mpb.SetBuffer("_UrgData", distanceDataBuffer);
+        mpb.SetVector("_UrgPos", urgPos);
+        mpb.SetVector("_UrgProps", urgProps);
+        mpb.SetVector("_AreaProps", areaSize);
+        sensingAreaRenderer.SetPropertyBlock(mpb);
     }
 }
---
+++ Assets/UrgVisualize/Shaders/VisualizeUrg.shader
@@ -0,0 +1,68 @@
+﻿Shader "Unlit/VisualizeUrg"
+{
+    Properties
+    {
+		_UrgPos ("sensor local position(x,y), urgAngle", Vector) = (0,0,0,0)
+		_UrgProps ("prop(steps, angleOffset(rad), angleDelta(rad))", Vector) = (0,0,0,0)
+		_AreaProps ("sensing area props(width, height)", Vector) = (1,1,0,0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+			#pragma target 5.0
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+				float2 lPos : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+			StructuredBuffer<float> _UrgData;
+			float4 _UrgPos,_UrgProps,_AreaProps;
+
+            v2f vert (appdata v)
+            {
+				v.vertex.xy *= _AreaProps.xy;
+
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.lPos = v.vertex.xy;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float2 vec = i.lPos - _UrgPos.xy;
+				float angle = atan2(vec.y,vec.x) - _UrgPos.z;
+				float dist = length(vec);
+
+				angle += (angle < -UNITY_PI)? 2*UNITY_PI:0;
+				angle -= (UNITY_PI < angle) ? 2*UNITY_PI:0;
+
+				float t = angle + _UrgProps.y;
+				t *= 1.0/_UrgProps.z;
+
+				int step = floor(t);
+				float data = _UrgData[step];
+
+                return dist < data;
+            }
+            ENDCG
+        }
+    }
+}
